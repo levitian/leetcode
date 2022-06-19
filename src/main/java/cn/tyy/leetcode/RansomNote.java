06dfbacf2f7f6d84b1f5bef1ca4e48e40adc35b9
tianli <1009910968@qq.com>
2022-06-19 19:29:14 +0800

    383. 赎金信

--- src/main/java/cn/tyy/leetcode/RansomNote.java
+++ src/main/java/cn/tyy/leetcode/RansomNote.java
@@ -2,6 +2,16 @@ package cn.tyy.leetcode;
 
 /**
  * leetcode 383. 赎金信
+ *
+ * 给你两个字符串：ransomNote 和 magazine ，判断 ransomNote 能不能由 magazine 里面的字符构成。
+ *
+ * 如果可以，返回 true ；否则返回 false 。
+ *
+ * magazine 中的每个字符只能在 ransomNote 中使用一次。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/ransom-note
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class RansomNote {
 
